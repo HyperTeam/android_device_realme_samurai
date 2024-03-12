05d8ef5b193091af8cd5f8f90a35b6201de8ad4a
alextrack2013 <alextrack2013@gmail.com>
2024-03-12 21:55:44 +0100

    samurai: Build `vendor.qti.hardware.camera.postproc@1.0` from source
    Change-Id: I9a3637878468375d4343c04ddde8f2458230b2e2

--- device.mk
+++ device.mk
@@ -155,7 +155,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libcamera_metadata_shim \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Charger
 PRODUCT_PACKAGES += \
